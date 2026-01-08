23659cfb7305f4e460c3398f98257ab048ac3d16
Arindam Sharma <arindamsharma693@gmail.com>
2026-01-08 16:48:04 +0530

    Update medusa-config.ts

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -1,27 +1,16 @@
 import { loadEnv, defineConfig } from "@medusajs/framework/utils"
-import path from "path"
 
 loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
-const modules: any[] = [];
-
-
-
 export default defineConfig({
   projectConfig: {
-    // Database
     databaseUrl: process.env.DATABASE_URL,
-
     http: {
-      // CORS
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
-
-      // Security
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
   },
-  modules,
 })
